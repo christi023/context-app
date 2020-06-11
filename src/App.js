0e866375274e79033f6c4e1a79ef924516310e83
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-06-11 10:44:03 +0200

    Add Form, Page Content,Theme and Language Contexts plus HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 // Component
 import Navbar from './Components/Navbar/Navbar';
-
+import Form from './Components/Form/Form';
+import PageContent from './Components/PageContent/PageContent';
+// contexts
+import { ThemeProvider } from './contexts/ThemeContext';
+import { LanguageProvider } from './contexts/LanguageContext';
 class App extends Component {
   render() {
-    return <Navbar />;
+    return (
+      <ThemeProvider>
+        <LanguageProvider>
+          <PageContent>
+            <Navbar />
+            <Form />
+          </PageContent>
+        </LanguageProvider>
+      </ThemeProvider>
+    );
   }
 }
 
