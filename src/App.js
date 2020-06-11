89ff3f5e3f1e161485c720b6a39c0d330cf72712
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-06-11 13:03:01 +0200

    Refactor all components to use hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 // Component
 import Navbar from './Components/Navbar/Navbar';
 import Form from './Components/Form/Form';
 import PageContent from './Components/PageContent/PageContent';
 // contexts
-import { ThemeProvider } from './contexts/ThemeContext';
+import ThemeProvider from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider>
-        <LanguageProvider>
-          <PageContent>
-            <Navbar />
-            <Form />
-          </PageContent>
-        </LanguageProvider>
-      </ThemeProvider>
-    );
-  }
-}
 
-export default App;
+export default function App() {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
+}
